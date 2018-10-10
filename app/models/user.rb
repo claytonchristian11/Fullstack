10204718c3fbee7dd93e5add5377537a4222897c
nmatison <nmatison@gmail.com>
2018-10-09 17:51:17 -0700

    Got through sessionForm components following benchbnb

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+
+  validates :username, :password_digest, :session_token, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader: password
+
+  after_initialize: ensure_session_token
+
+  def self.find_by_credentials(username, pw)
+    user = User.find_by(username: username)
+    if user && user.is_password?(pw)
+      user
+    else
+      nil
+    end
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+  
+end
---
+++ frontend/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <header>
+      <h1>CloudSounds</h1>
+      <GreetingContainer />
+    </header>
+  </div>
+);
+
+export default App;
