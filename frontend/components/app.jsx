e0c3256c9397e091659af63458467a8396b69bc7
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-10 18:01:02 -0700

    finish all user auth except modal styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -21,10 +21,12 @@ const App = () => (
 
       <main className='main-body'>
         <div className='main-body-adtext'>
-          <h1 className='main-body-adtext-go+'>Discover more with CloudSounds Cloud+</h1>
-          <p className='main-body-adtext-text'>CloudSounds cloud+ lets you listen offline, ad-free, with over 150 million clouds - and growing.</p>
-          <button className='main-body-adtext-b1'>Learn More</button>
-          <button className='main-body-adtext-b2'>Try it free for 30 days</button>
+          <p className='main-body-adtext-go'>Discover more with CloudSounds Cloud+</p>
+          <p className='main-body-adtext-text'>CloudSounds cloud+ lets you listen offline, ad-free, with over 150 million songs.</p>
+          <div className='main-body-buttons'>
+            <button className='main-body-adtext-b1'>Learn More</button>
+            <button className='main-body-adtext-b2'>Try it free for 30 days</button>
+          </div>
         </div>
 
       </main>
